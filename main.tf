a765916307ffb29cde3e985d0d954c50f5bb7f0e
Noah Schill <noahrschill@gmail.com>
2024-08-21 13:53:20 -0700

    deploys a single VM Successfully

---
+++ main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc3"
+  }
+}
+}
+
+
+provider "proxmox" {
+  pm_api_url = var.PM_API_URL
+  pm_api_token_id = var.PM_API_TOKEN_ID
+  pm_api_token_secret = var.PM_API_TOKEN_SECRET
+  pm_tls_insecure = true
+  pm_debug = true
+  pm_log_enable = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
+}
+
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+    name = "terraform-test-vm"
+    desc = "A test for using terraform and cloudinit"
+
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "pve"
+
+    # The destination resource pool for the new VM
+    pool = "closed"
+
+    # The template name to clone this vm from
+    clone = "ubuntu-cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    scsihw = "lsi"
+
+    # Setup the disk
+    disks {
+        ide {
+            ide3 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+        virtio {
+            virtio0 {
+                disk {
+                    size            = 32
+                    cache           = "writeback"
+                    storage         = "ceph-storage-pool"
+                    iothread        = true
+                    discard         = true
+                }
+            }
+        }
+    }
+
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+        tag = 256
+    }
+
+    # Setup the ip address using cloud-init.
+    boot = "order=virtio0"
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.10.20/24,gw=192.168.10.1"
+
+    # TODO create keypair on host... silly
+    sshkeys = <<EOF
+    ssh-rsa 9182739187293817293817293871== user@pc
+    EOF
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "PM_API_URL" {
+   type=string
+}
+variable "SSH_PUBLIC_KEY" {
+   type=string
+}
+variable "PM_API_TOKEN_ID" {
+   type=string
+}
+variable "PM_API_TOKEN_SECRET" {
+   type=string
+}
