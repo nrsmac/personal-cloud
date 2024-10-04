175f0f73b4e09b91c4cb38a9b7aa925b5d49f6aa
Noah Schill <noahrschill@gmail.com>
2024-10-04 10:46:29 -0700

    tails + personal

--- main.tf
+++ main.tf
@@ -92,10 +92,109 @@ resource "proxmox_virtual_environment_vm" "barracuda" {
     type = "virtio-gl"
   }
 }
-resource "proxmox_virtual_environment_download_file" "nix_cloud_image" {
+resource "proxmox_virtual_environment_vm" "personal" {
+  count = 1
+  name = "personal"
+  node_name = var.TARGET_NODE
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "${var.BASE_MASTER_IP}8/${var.NETWORK_CIDR}"
+        gateway = var.IP_GATEWAY 
+      }
+    }
+
+    user_account {
+      username = var.CI_USER
+      password = var.CI_PASSWORD
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  cpu {
+    cores = var.MASTER_CPU_CORES 
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  disk {
+    datastore_id = var.STORAGE_NAME
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 15  # GB
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  vga {
+    type = "virtio-gl"
+  }
+}
+resource "proxmox_virtual_environment_vm" "browsing" {
+  count = 1
+  name = "browsing"
+  node_name = var.TARGET_NODE
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "${var.BASE_MASTER_IP}9/${var.NETWORK_CIDR}"
+        gateway = var.IP_GATEWAY 
+      }
+    }
+
+    user_account {
+      username = var.CI_USER
+      password = var.CI_PASSWORD
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  cpu {
+    cores = var.MASTER_CPU_CORES 
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  disk {
+    datastore_id = var.STORAGE_NAME
+    file_id      = proxmox_virtual_environment_download_file.tails_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 15  # GB
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  vga {
+    type = "virtio-gl"
+  }
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local" 
   node_name    = var.TARGET_NODE 
-
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
 }
+resource "proxmox_virtual_environment_download_file" "tails_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local" 
+  node_name    = var.TARGET_NODE 
+  url = "https://download.tails.net/tails/stable/tails-amd64-6.7/tails-amd64-6.7.iso"
+}
+
